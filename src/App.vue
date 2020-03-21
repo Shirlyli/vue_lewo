61dbe23f554751eae6b64d9918946bec65c60e06
shirly007240 <120989756@qq.com>
2020-03-21 10:37:40 +0800

    完成了组件切换特效

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <transition>
+      <router-view></router-view>
+    </transition>
     <TabbBar></TabbBar>
   </div>
 </template>
@@ -11,13 +13,31 @@ export default {
   name: 'app',
   components: {
     TabbBar
+  },
+  beforeCreate () {
+    this.$router.push('/')
   }
 }
 </script>
 
 <style>
-*{
-  margin:0;
+* {
+  margin: 0;
   padding: 0;
+}
+.v-enter{
+  opacity: 0;
+  transform: translateX(100%);
+}
+
+.v-leave-to{
+  opacity: 0;
+  transform: translateX(-100%);
+  position: absolute;
+}
+
+.v-enter-active,
+.v-leave-active{
+  transition: all 0.5s ease;
 }
 </style>
