3377c8e373ee642555779263f6c1b9bac217ac69
shirly007240 <120989756@qq.com>
2020-03-21 14:14:18 +0800

    shopcar has done

--- src/views/ShopCar.vue
+++ src/views/ShopCar.vue
@@ -1,5 +1,14 @@
 <template>
-  <div></div>
+  <div>
+    <mt-header fixed title="购物车"></mt-header>
+    <div class="img">
+      <img src="../../public/image/emptyCart.jpg" alt />
+      <div>
+        <span>空空如也</span>
+        <span>随便逛逛></span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +16,30 @@ export default {
   name: 'shopcar'
 }
 </script>
+
+<style lang="scss" scoped>
+$pink: rgb(240, 66, 217);
+.mint-header {
+  background-color: white;
+  color: $pink;
+  font-size: 1rem;
+}
+.img {
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  margin-left: -4rem;
+  margin-top: -8rem;
+  img {
+    max-width: 8rem;
+  }
+  span{
+    font-size: .6rem;
+    font-weight: 300;
+    margin-right: .8rem;
+  }
+  span:nth-child(2){
+    color: $pink;
+  }
+}
+</style>
