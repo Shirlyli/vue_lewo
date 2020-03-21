e71dae9894a72e49c5ec4d0ff7c24d908842cfb2
shirly007240 <120989756@qq.com>
2020-03-21 09:17:23 +0800

    rank.vue has been done

--- src/views/Rank.vue
+++ src/views/Rank.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <img src="../../public/image/rank/rank_1.jpg" alt="">
+    <div class="main">
+      <img src="../../public/image/rank/rank_1.jpg" alt />
+      <img src="../../public/image/rank/rank_2.jpg" alt />
+      <img src="../../public/image/rank/rank_3.jpg" alt />
+      <img src="../../public/image/rank/rank_4.jpg" alt />
+      <img src="../../public/image/rank/rank_5.jpg" alt />
+    </div>
   </div>
 </template>
 
@@ -11,7 +17,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  img{
+.main {
+  width: 100%;
+  height: calc(100vh - 4rem);
+  overflow: scroll;
+  img {
     width: 100%;
+    display: block;
   }
+}
 </style>
